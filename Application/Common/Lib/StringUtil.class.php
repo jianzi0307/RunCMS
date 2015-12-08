3d314e9846b0bf586d92049e0299fa921eaa5d4b
jianzi0307 <jianzi0307@gmail.com>
2015-12-08 22:21:24 +0800

    StringUtil class

--- Application/Common/Lib/StringUtil.class.php
+++ Application/Common/Lib/StringUtil.class.php
@@ -8,7 +8,23 @@
 
 namespace Lib;
 
-
-class StringUtil {
+class StringUtil
+{
+    /**
+     * 用户名是否符合格式
+     * 用户名为大写字母小写字母或下划线,并以字母开头,长度为6-20.
+     * ^[a-zA-Z][\w\d_]{5,19}
+     * @param $username
+     * @return bool
+     */
+    public static function isUsername($username)
+    {
+        $ruler = '/^[a-zA-Z][\w\d_]{5,19}/';
+        if (preg_match($ruler, $username)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
